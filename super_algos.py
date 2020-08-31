0add5e325c0f08e25d7180f6ba51a5ad6a664857
Ndumasi <c4r5s3@c4r5s3.co.za>
2020-08-31 17:53:59 +0200

    Complete

--- super_algos.py
+++ super_algos.py
@@ -1,16 +1,48 @@
+import random
+
+#element = ['a',100,'b',4,-5]
+character_set = ['a','b']
+n = 3
 
 def find_min(element):
-    """TODO: complete for Step 1"""
-    pass
+    for i in element:
+        if type(i) != int:
+            return -1
+    if len(element) == 0:
+        return -1
+    elif len(element) == 1:
+        return element[0]
+
+    elif len(element) != 1:
+        if element[0] < element[1]:
+            del element[1]
+            print(element)
+            find_min(element)
+        else:
+            random.shuffle(element)
+            print(element)
+            find_min(element)
+    return element[0]
 
 
 def sum_all(element):
-    """TODO: complete for Step 2"""
-    pass
+    for i in element:
+        if type(i) != int:
+            return -1
+    if len(element) == 1:
+        return element[0]
+    if len(element) == 0:
+        return -1
+    else:
+        element[0] += element[1]
+        del element[1]
+        print(element)
+        sum_all(element)
+    return element[0]
 
 
 def find_possible_strings(character_set, n):
-    """TODO: complete for Step 3"""
-    pass
+    for i in range(len(text)):
 
-
+if __name__ == "__main__":
+    find_possible_strings(character_set, n)
